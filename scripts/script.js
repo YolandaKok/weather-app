051743c573e5c786c7aa0c3daeb0d67f57c55e95
YolandaKok <yiola8c@gmail.com>
2016-11-15 19:57:00 +0200

    changed js

--- scripts/script.js
+++ scripts/script.js
@@ -30,34 +30,34 @@ $(document).ready(function() {
           var icon = data.currently.icon;
           switch (icon) {
             case "clear-day":
-              $.(".weather-icon1").html("<i class='wi wi-day-sunny'></i>");
+              $(".weather-icon1").html("<i class='wi wi-day-sunny'></i>");
               break;
             case "clear-night":
-              $.(".weather-icon1").html("<i class='wi wi-night-clear'></i>");
+              $(".weather-icon1").html("<i class='wi wi-night-clear'></i>");
               break;
             case "rain":
-              $.(".weather-icon1").html("<i class='wi'></i>");
+              $(".weather-icon1").html("<i class='wi'></i>");
               break;
             case "snow":
-              $.(".weather-icon1").html("<i class='wi'></i>");
+              $(".weather-icon1").html("<i class='wi'></i>");
               break;
             case "sleet":
-              $.(".weather-icon1").html("<i class='wi'></i>");
+              $(".weather-icon1").html("<i class='wi'></i>");
               break;
             case "wind":
-              $.(".weather-icon1").html("<i class='wi'></i>");
+              $(".weather-icon1").html("<i class='wi'></i>");
               break;
             case "fog":
-              $.(".weather-icon1").html("<i class='wi'></i>");
+              $(".weather-icon1").html("<i class='wi'></i>");
               break;
             case "cloudy":
-              $.(".weather-icon1").html("<i class='wi wi-day-cloudy'></i>");
+              $(".weather-icon1").html("<i class='wi wi-day-cloudy'></i>");
               break;
             case "partly-cloudy-day":
-              $.(".weather-icon1").html("<i class='wi'></i>");
+              $(".weather-icon1").html("<i class='wi'></i>");
               break;
             case "partly-cloudy-night":
-              $.(".weather-icon1").html("<i class='wi wi-night-cloudy'></i>");
+              $(".weather-icon1").html("<i class='wi wi-night-cloudy'></i>");
               break;
 
           }
